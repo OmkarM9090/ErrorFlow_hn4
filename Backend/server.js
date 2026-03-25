d483a6d065fd6cdc7ae180b25b88e0c73ad60f93
YashRaut24 <yashcodezzzz24@gmail.com>
2026-03-25 11:53:49 +0530

    Intialized backend server

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const scrape = require('./scraper');
+
+const app = express();
+app.use(cors());
+
+app.get('/scan', async (req, res) => {
+    const url = req.query.url;
+    const data = await scrape(url);
+    res.json(data);
+});
+
+app.listen(5000, () => console.log("Server running"));
